e7dc58a5b8ec8fa16ac11804632969cf4e74859b
George.Garin <geogarin@gmail.com>
2021-05-14 01:36:02 +0300

    fix

--- MainInterface.py
+++ MainInterface.py
@@ -1,7 +1,9 @@
 from Channel import ChannelView
 from MainModel import MainModel
+from Config import BUTTON_START_TEST
+from stylesheets import ButtonStartStyle,ButtonStopStyle,MenuButtonStyle
 
-from PyQt5.QtWidgets import QDialog,QWidget,QGridLayout,QProgressBar,QPushButton
+from PyQt5.QtWidgets import QDialog,QWidget,QGridLayout,QProgressBar,QPushButton,QHBoxLayout
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt,pyqtSlot
 
@@ -17,35 +19,56 @@ class MainInterface(QWidget):
         self.channels = []
         self.mainLayout = QGridLayout()
 
-        self.bu = QPushButton('Set')
-        self.mainLayout.addWidget(self.bu,1,0,1,1)
+        self.menu = QHBoxLayout()
+
+
+        self.setupButton = QPushButton(self._model.buttonSetupName)
+        self.setupButton.setStyleSheet(MenuButtonStyle)
+        self.manualButton = QPushButton(self._model.buttonManualName)
+        self.manualButton.setStyleSheet(MenuButtonStyle)
+
+        self.menu.addWidget(self.setupButton)
+        self.menu.addWidget(self.manualButton)
+
+        self.mainLayout.addLayout(self.menu,0,0,1,4,Qt.AlignRight)
 
         for i in range(self._model.channelsQuantity):
             #print(i)
-            #self._model.channelModels[i] = ChannelModel(PINS_ABS[i],PINS_DIF[i])
             ch = ChannelView(self._model.channelModels[i])
             self.channels.append(ch)
-            #self.mainLayout.addWidget(self.channels[i].group,2,i,1,1)
-            #self.mainLayout.addLayout(self.channels[i].group,2,i,1,1)
+            
             self.mainLayout.addWidget(self.channels[i],2,i,1,1,Qt.AlignVCenter)
 
-        #self.progressBar = QProgressBar()
-        #self.progressBar.setRange(0, 10000)
-        #self.progressBar.setValue(5000) 
 
-        self.font = QtGui.QFont("Times",75)
         self.startTestButton = QPushButton(self._model.startTestButtonName)
-        self.startTestButton.setFont(self.font)
-
-
+        self.startTestButton.setObjectName("StartTestButton")
+        self.startTestButton.setStyleSheet(ButtonStartStyle)
+ 
+ 
         self.mainLayout.addWidget(self.startTestButton,3,0,1,self._model.channelsQuantity)
-        #self.mainLayout.addWidget(self.progressBar,5,0,1,channelsQty)
         
         self.setLayout(self.mainLayout)
 
         self._model.startTestButtonNameChanged.connect(self.onStartTestButtonNameChanged)
         self.startTestButton.clicked.connect(lambda: self._model.startTestButtonPressed())
 
+        # >>
+        self.setupButton.clicked.connect(self.stpBtn)
+
+    def stpBtn(self):
+        dlg = QDialog(self)
+        dlg.setWindowTitle("HELLO!")
+        
+        dlg.exec_()
+    # <<
+
+
+
+
     @pyqtSlot(str)
     def onStartTestButtonNameChanged(self,value):
         self.startTestButton.setText(value)
+        if (value == BUTTON_START_TEST):
+            self.startTestButton.setStyleSheet(ButtonStartStyle)
+        else:
+            self.startTestButton.setStyleSheet(ButtonStopStyle)
