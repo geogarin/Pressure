186f284d630526ca94e1cb3909eda75f74c78f30
George.Garin <geogarin@gmail.com>
2021-06-09 02:31:59 +0300

    usb

---
+++ Test/usbDetect.py
@@ -0,0 +1,46 @@
+import usb
+from usb.core import USBError
+
+### Some auxiliary functions ###
+def _clean_str(s):
+    '''
+    Filter string to allow only alphanumeric chars and spaces
+
+    @param s: string
+    @return: string
+    '''
+
+    return ''.join([c for c in s if c.isalnum() or c in {' '}])
+
+
+def _get_dev_string_info(device):
+    '''
+    Human readable device's info
+
+    @return: string
+    '''
+    str_info = ''
+    try:
+        str_info = _clean_str(usb.util.get_string(device, 256, 2))
+        str_info += ' ' + _clean_str(usb.util.get_string(device, 256, 3))
+        return str_info
+    except USBError:
+        return str_info
+
+
+def get_usb_devices():
+    '''
+    Get USB devices
+
+    @return: list of tuples (dev_idVendor, dev_idProduct, dev_name)
+    '''
+
+    return [(device.idVendor, device.idProduct, _get_dev_string_info(device),device.serial_number) 
+                for device in usb.core.find(find_all=True)
+                    if device.idProduct > 2
+                    ]
+
+if __name__=='__main__':
+    u = get_usb_devices()
+    print(u)
+    print('qqq')
